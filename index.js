9cac7d44ce2590e9f00ad2b8a1972d46a4c81595
Alpha <alpha.likembe@student.moringaschool.com>
2023-07-14 06:02:35 +0300

    Test Complete

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let result
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (distance <= 400){
+    result = 'This one is on me!'
+  }else{
+    if (distance <=2500) {
+      result = 'I will gladly take your thirty bucks.'
+    }else{
+      if (distance >2500){
+        result = 'No can do.'
+      }
+    }
+  }
+  return result
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet (2001));
+console.log(scuberGreetingForFeet(2501))
 
-function switchOnCharmFromTip(){
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
+  console.log(ternaryCheckCity(NYC));
+  console.log(ternaryCheckCity('Pittsburgh'))
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
+console.log(tip);
